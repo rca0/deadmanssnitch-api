96a4ede46bbad5ffddf25092073c0a8430722696
Ruan Arcega <ruanarcega@gmail.com>
2022-12-12 00:56:40 -0300

    feat: create an api to handle the post requests

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/rca0/deadmanssnitch-api
 go 1.18
 
 require github.com/google/go-querystring v1.1.0
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,29 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/rca0/deadmanssnitch-api/deadmanssnitch"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	apikey := os.Getenv("DEADMANSSNITCH_APIKEY")
+	if apikey == "" {
+		log.Fatal("You should define the DEADMANSSNITCH_APIKEY environment variable, set and try again... :)")
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api", CreateSnitch).Methods("POST")
+
+	log.Printf("Starting server http://0.0.0.0:%s...", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), router)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CreateSnitch(w http.ResponseWriter, r *http.Request) {
+	var snitch deadmanssnitch.Snitch
+	err := json.NewDecoder(r.Body).Decode(&snitch)
+	if err != nil {
+		log.Printf("[x] invalid request payload: %s", err)
+		return
+	}
+	defer r.Body.Close()
+
 	client, err := deadmanssnitch.NewClient(&deadmanssnitch.Config{
 		ApiKey: os.Getenv("DEADMANSSNITCH_APIKEY"),
 	})
-
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("[x] error when create new snitch client: %s", err)
+		return
 	}
 
-	resp, err := client.Snitch.NewSnitch(&deadmanssnitch.Snitch{
-		Name:     "testing-api",
-		Interval: "daily",
-	})
+	resp, err := client.Snitch.NewSnitch(&snitch)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("[x] could not create new snitch: %s", err)
+		return
 	}
+
+	w.WriteHeader(http.StatusCreated)
 
 	fmt.Printf("Successfully created [%s]", resp.Name)
 }
