b34f7dd2572b6a0155a48b001c6a73e12c5c14a0
Ruan Arcega <ruanarcega@gmail.com>
2022-12-14 02:23:16 -0300

    fix: controllers names in routes

--- routes/routes.go
+++ routes/routes.go
@@ -13,10 +13,10 @@ func SnitchRoute(router *mux.Router) {
 	router.HandleFunc("/api/snitches/{token}", controllers.GetSnitch()).Methods("GET")
 
 	// to update a snitch, send PATCH request
-	router.HandleFunc("/api/snitches/{token}", controllers.GetSnitch()).Methods("PATCH")
+	router.HandleFunc("/api/snitches/{token}", controllers.UpdateSnitch()).Methods("PATCH")
 
 	// delete snitch
-	router.HandleFunc("/api/snitches/{token}", controllers.GetSnitch()).Methods("DELETE")
+	router.HandleFunc("/api/snitches/{token}", controllers.DeleteSnitch()).Methods("DELETE")
 
 	// list snitches
 	router.HandleFunc("/api/snitches", controllers.GetSnitches()).Methods("GET")
