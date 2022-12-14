cedf3f9d669b624ef2e7ee76f5dd9243e08b75d0
Ruan Arcega <ruanarcega@gmail.com>
2022-12-14 01:58:20 -0300

    fix: api deadmanssnitch paths

--- routes/routes.go
+++ routes/routes.go
@@ -7,16 +7,16 @@ import (
 
 func SnitchRoute(router *mux.Router) {
 	// create new snitch
-	router.HandleFunc("/api/snitch", controllers.CreateSnitch()).Methods("POST")
+	router.HandleFunc("/api/snitches", controllers.CreateSnitch()).Methods("POST")
 
 	// get snitch
-	router.HandleFunc("/api/snitch/{token}", controllers.GetSnitch()).Methods("GET")
+	router.HandleFunc("/api/snitches/{token}", controllers.GetSnitch()).Methods("GET")
 
 	// to update a snitch, send PATCH request
-	router.HandleFunc("/api/snitch/{token}", controllers.GetSnitch()).Methods("PATCH")
+	router.HandleFunc("/api/snitches/{token}", controllers.GetSnitch()).Methods("PATCH")
 
 	// delete snitch
-	router.HandleFunc("/api/snitch/{token}", controllers.GetSnitch()).Methods("DELETE")
+	router.HandleFunc("/api/snitches/{token}", controllers.GetSnitch()).Methods("DELETE")
 
 	// list snitches
 	router.HandleFunc("/api/snitches", controllers.GetSnitches()).Methods("GET")
